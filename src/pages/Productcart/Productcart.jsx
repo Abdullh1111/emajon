71b157dc5dbbcbc1709b33252a66ecaa88afd326
Abdullh1111 <abdullah4474032@gmail.com>
2024-03-20 08:32:28 +0600

    fix error

--- src/pages/Productcart/Productcart.jsx
+++ src/pages/Productcart/Productcart.jsx
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types"
-import { Link, useNavigate } from "react-router-dom";
+import {  useNavigate } from "react-router-dom";
 
 const Productcart = ({product}) => {
     const {id,title,thumbnail,price} = product;
